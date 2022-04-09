47ff2e1095b5ef75cd386d07d3520f726c706df8
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-04-09 14:49:06 +0200

    chore: add file to replace bad timestamps

---
+++ replaceWrongTimestampInStr.js
@@ -0,0 +1,31 @@
+const chalk = require("chalk");
+var fs = require("fs");
+const { execCommand, extractFileNames } = require("./execCommand");
+
+const replaceWrongTimeStampInSrt = (fileName) => {
+  fs.readFile(fileName, "utf8", function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var result = data.replace(/(\d{2})[\s.]?(\d{3})/g, "$1,$2");
+
+    fs.writeFile(fileName.replace(".pl", ""), result, "utf8", function (err) {
+      console.log(chalk.bgGreenBright(chalk.black(` ${fileName} adjusted \n`)));
+      if (err) return console.log(err);
+    });
+    execCommand(`rm -rf ${fileName}`, () =>
+      console.log(chalk.yellow(`removed ${fileName}\n`))
+    );
+  });
+};
+
+const replaceAllWrongTimestamps = () =>
+  execCommand("ls", (stdout) => {
+    const allSrtPLFiles = extractFileNames(stdout, undefined, ".srt").filter(
+      (file) => file.match(/.pl.srt/)
+    );
+
+    allSrtPLFiles.forEach((movie) => replaceWrongTimeStampInSrt(movie));
+  });
+
+replaceAllWrongTimestamps();
---
+++ translateSrt.js
@@ -0,0 +1,1 @@
+// trans -i Pinocchio.E01.DC.BluRay\[MkvDrama.com\].srt -o Pinocchio.E01.DC.BluRay\[MkvDrama.com\].pl.srt -t pol -b -show-original n
