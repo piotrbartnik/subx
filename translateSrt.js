c1229bac09ff2123bfe907a977e3fd9a7fb357c3
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-04-10 10:06:43 +0200

    chore: adjust wording in translate file

--- translateSrt.js
+++ translateSrt.js
@@ -1,25 +1,23 @@
-// trans -i Pinocchio.E01.DC.BluRay\[MkvDrama.com\].srt -o Pinocchio.E01.DC.BluRay\[MkvDrama.com\].pl.srt -t pol -b -show-original n
-
 const chalk = require("chalk");
 var fs = require("fs");
 const { execCommand, extractFileNames } = require("./execCommand");
 
-const tranlateFile = (fileName) => {
+const translateFile = (fileName) => {
   console.log(fileName);
   execCommand(
     `trans -i ${fileName} -o ${fileName.replace(
       ".srt",
       ".pl.srt"
     )} -t pol -b -show-original n`,
-    () => console.log(chalk.greenBright(`translated ${fileName}\n`))
+    () => console.log(chalk.greenBright(`File ${fileName} was translated \n`))
   );
 };
 
 const translateAllFiles = () =>
   execCommand("ls", (stdout) => {
-    const allSrtPLFiles = extractFileNames(stdout, undefined, ".srt");
+    const allSrtSubtitleFiles = extractFileNames(stdout, undefined, ".srt");
 
-    allSrtPLFiles.forEach((subtitlesFile) => tranlateFile(subtitlesFile));
+    allSrtSubtitleFiles.forEach(translateFile);
   });
 
 translateAllFiles();
