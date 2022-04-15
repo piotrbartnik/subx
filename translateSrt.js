d778de67aeb84f1c70e40dc45e547c0c4860f7ee
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-04-15 07:50:24 +0200

    chore: add input language argument to translation function

--- translateSrt.js
+++ translateSrt.js
@@ -2,13 +2,13 @@ const chalk = require("chalk");
 var fs = require("fs");
 const { execCommand, extractFileNames } = require("./utils");
 
-const translateFile = (fileName) => {
+const translateFile = (fileName, inputLanguage) => {
   console.log(fileName);
   execCommand(
     `trans -i ${fileName} -o ${fileName.replace(
       ".srt",
       ".pl.srt"
-    )} -t pol -b -show-original n`,
+    )} -l ${inputLanguage} -t pol -b -show-original n`,
     () => console.log(chalk.greenBright(`File ${fileName} was translated \n`))
   );
 };
@@ -17,7 +17,7 @@ const translateAllFiles = () =>
   execCommand("ls", (stdout) => {
     const allSrtSubtitleFiles = extractFileNames(stdout, undefined, ".srt");
 
-    allSrtSubtitleFiles.forEach(translateFile);
+    allSrtSubtitleFiles.forEach(translateFile, inputLanguage);
   });
 
-translateAllFiles();
+translateAllFiles((inputLanguage = "eng"));
