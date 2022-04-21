278c301c3e444a7678f75ade3fd7c59cfef7b852
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-04-21 21:23:31 +0200

    chore: move all files to src directory

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const { program } = require("commander");
-const { runExtraction } = require("../extractSubtitles");
+const { runExtraction } = require("../src/extractSubtitles");
 
 program
   .command("extract-subtitles")
