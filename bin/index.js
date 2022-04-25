6f731a65c8b3af93e2a51b4729d9f25d90ccc0f0
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-04-25 21:40:03 +0200

    chore: add description and version to index cli

--- bin/index.js
+++ bin/index.js
@@ -3,12 +3,14 @@
 const { program } = require("commander");
 const { resolve } = require("path");
 
-program.command(
-  "extract-subtitles",
-  "Extract subtitle in srt from a given track",
-  {
+program
+  .name("subx")
+  .version("0.1.0")
+  .description(
+    "Extract, translate and adjust srt subitle files from mkv movies"
+  )
+  .command("extract-subtitles", "Extract subtitle in srt from a given track", {
     executableFile: resolve(__dirname, "../src/extractSubtitles/index.js"),
-  }
-);
+  });
 
 program.parse();
