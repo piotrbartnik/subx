363e4c6d561b37b0c5676d7b8b28191a398cff95
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-05-09 21:37:57 +0200

    chore: add initial implemenation of inquirer to install the software needed in prerequisites

--- src/prerequisites/prerequisites.js
+++ src/prerequisites/prerequisites.js
@@ -1,6 +1,19 @@
 const { execCommand } = require("../utils");
+const inquirer = require("inquirer");
 
-const neededSoftware = ["mkvinfo", "mkvextract", "vobsub2srt", "trans"];
+const neededSoftware = ["mkvinfofd" /*"mkvextract", "vobsub2srt", "trans"*/];
+
+const questions = [
+  {
+    type: "list",
+    name: "installation",
+    message: `Do you want to install it?`,
+    choices: ["Yes", "No"],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+];
 
 const checkPrerequisites = () => {
   neededSoftware.forEach(async (software) => {
@@ -8,7 +21,13 @@ const checkPrerequisites = () => {
       `which ${software}`,
       () => console.log(`${software} is installed`),
       true,
-      () => console.log(`${software} is not installed`)
+      () => {
+        console.log(`${software} is not installed`);
+        inquirer.prompt(questions).then((answers) => {
+          console.log("\nOrder receipt:");
+          console.log(JSON.stringify(answers, null, "  "));
+        });
+      }
     );
   });
 };
