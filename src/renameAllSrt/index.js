725bf84945c5ad0b2589a1a35009060a2e1a25ae
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-07-18 19:16:20 +0200

    chore: add patter option to rename all files script

--- src/renameAllSrt/index.js
+++ src/renameAllSrt/index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
+const { program } = require("commander");
 
 const { renameAllSubtitles } = require("./renameAllSrt");
 
 const renameAllSubtitlesRun = () => {
-  renameAllSubtitles();
+  program.option(
+    "-p, --file-pattern <string>",
+    "File patter from file name to extract usuall as episode number"
+  );
+
+  program.parse();
+
+  const programOptions = program.opts();
+
+  renameAllSubtitles(programOptions?.["filePattern"]);
 };
 
 renameAllSubtitlesRun();
