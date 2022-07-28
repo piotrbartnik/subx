889d8a9af707b95ddd30777190f1fe301490012b
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-07-28 20:05:53 +0200

    chore: gulp loop first attempt

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,26 @@ const gulp = require("gulp");
 const uglify = require("gulp-uglify");
 const pipeline = require("readable-stream").pipeline;
 
-gulp.task("compress", function () {
-  return pipeline(gulp.src("bin/*.js"), uglify(), gulp.dest("dist/bin"));
+gulp.task("build", () => {
+  const dirs = [
+    "bin",
+    "src",
+    "src/adjustSubitles",
+    "src/adjustTImestamps",
+    "src/extractSubtitles",
+    "src/prerequisites",
+    "src/renameAllSrt",
+    "src/translateSrt",
+    "src/vobSubToSrt",
+  ];
+
+  dirs.forEach((dir) => {
+    gulp.task(`compress ${dir}`, function () {
+      return pipeline(
+        gulp.src(`${dir}/*.js`),
+        uglify(),
+        gulp.dest(`dist/${dir}`)
+      );
+    });
+  });
 });
