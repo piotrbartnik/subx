d43684be62e9dc25e37cfaf9d1d23d093a94c729
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-07-30 19:37:39 +0200

    chore: add all js files to dist directory minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,9 @@
 const gulp = require("gulp");
 const uglify = require("gulp-uglify");
-const pipeline = require("readable-stream").pipeline;
 
-gulp.task("build", () => {
-  const dirs = [
-    "bin",
-    "src",
-    "src/adjustSubitles",
-    "src/adjustTImestamps",
-    "src/extractSubtitles",
-    "src/prerequisites",
-    "src/renameAllSrt",
-    "src/translateSrt",
-    "src/vobSubToSrt",
-  ];
-
-  dirs.forEach((dir) => {
-    gulp.task(`compress ${dir}`, function () {
-      return pipeline(
-        gulp.src(`${dir}/*.js`),
-        uglify(),
-        gulp.dest(`dist/${dir}`)
-      );
-    });
-  });
+gulp.task("build", function () {
+  return gulp
+    .src(["./**/*.js", "!./node_modules/**/*.js"])
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"));
 });
