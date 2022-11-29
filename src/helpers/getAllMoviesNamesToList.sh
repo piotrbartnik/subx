3868cec370bd3fba610aaf191e4f14ccd697c3bc
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-11-29 22:42:38 +0100

    chore: extend shell script for creating list of files

--- src/helpers/getAllMoviesNamesToList.sh
+++ src/helpers/getAllMoviesNamesToList.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for filename in *.mp4; do
+
+if [[ ${#1} == 0 ]]; then
+    echo "No file extension provided like mkv. Please provide one"
+fi
+
+echo "Creating filenames.txt"
+
+for filename in *.$1; do
     [ -e "$filename" ] || continue
-    echo "${filename//mp4/"srt"}" >> fileNames.txt
+    echo "${filename//$1/"srt"}" >> fileNames.txt
 done
