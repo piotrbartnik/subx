5999aae32d67e3ea8f463f1db4892f3605c7a9c1
Piotr Bartnik <piotr.bartnik@hotmail.com>
2023-02-17 19:59:20 +0100

    chore: add possibility to extract subs from mp4 files

--- src/extractSubtitles/index.js
+++ src/extractSubtitles/index.js
@@ -4,16 +4,18 @@ const { program } = require("commander");
 const { runExtraction } = require("./extractSubtitles");
 
 const extractSubtitlesRun = () => {
-  program.option(
-    "-t, --track <number>",
-    "track of subtitle in mkv file. Defaults to 2. Example: subx extract-subtitles -t 5"
-  );
+  program
+    .option(
+      "-t, --track <number>",
+      "track of subtitle in mkv file. Defaults to 2. Example: subx extract-subtitles -t 5"
+    )
+    .option("-mpeg, --mpeg <boolean>", "Extract from mp4 file with ffmpeg");
 
   program.parse();
 
   const programOptions = program.opts();
 
-  runExtraction(programOptions.track);
+  runExtraction(programOptions.track, programOptions.mpeg);
 };
 
 extractSubtitlesRun();
